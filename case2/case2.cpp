9cc00909f683f72d62b01a521eeb31f08a4b6c7e
ReshmaTamma <63240728+ReshmaTamma@users.noreply.github.com>
2020-04-21 12:52:02 +0530

    Add files via upload

--- case2/case2.cpp
+++ case2/case2.cpp
@@ -1,1 +1,44 @@
-
+/*Purpose 	:Program to find the sum of square of given number
+Language	:C++
+Author  	: Reshma Tamma
+Date    	: 10/04/2020
+*/
+#include<iostream>
+#include<string.h>
+using namespace std;
+class square
+{
+	private:
+		int number ;
+	public:
+		
+		 int sum (int inumber)
+		{
+		number=inumber;
+		int sum =0 ,i=1 ;
+		for(i=1;i<=inumber;i++)   // loop for calculating the sum of squares
+		 {
+		 	 sum=sum+(i*i) ;
+		 }
+		 cout<<"sum is"<<sum<<endl;
+}
+};
+int main(int argc, char* argv[])
+{
+    if(argc>=2)
+    {
+	    if(strcmp(argv[1],"-h")==0)					//if loop for comparing the input string with "-h"
+	    {
+            cout<<"Usage:find the sum of squares of nth term"<<endl;
+            
+	    }
+    }
+	else
+	{
+		int n ;
+		cout<<"enter the number "<<endl ;
+		cin>>n ;
+		square s;
+		s.sum(n);  //calling the function
+	}	
+}
